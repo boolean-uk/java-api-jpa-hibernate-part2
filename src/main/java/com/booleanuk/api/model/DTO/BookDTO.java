46b859db05cc54d35f82acd7f986d27b587c4837
Sebastian Andrisan <sebyyyandr@gmail.com>
2023-06-06 15:19:38 +0300

    dto simple models

---
+++ src/main/java/com/booleanuk/api/model/DTO/BookDTO.java
@@ -0,0 +1,51 @@
+package com.booleanuk.api.model.DTO;
+public class BookDTO {
+
+    private Integer id;
+    private String title;
+    private String genre;
+    private Integer author_id;
+    private Integer publisher_id;
+
+
+    //getters and setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Integer getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(Integer author_id) {
+        this.author_id = author_id;
+    }
+
+    public Integer getPublisher_id() {
+        return publisher_id;
+    }
+
+    public void setPublisher_id(Integer publisher_id) {
+        this.publisher_id = publisher_id;
+    }
+}
---
+++ src/main/java/com/booleanuk/api/model/DTO/PublisherDTO.java
@@ -0,0 +1,34 @@
+package com.booleanuk.api.model.DTO;
+
+public class PublisherDTO {
+
+    private Integer id;
+    private String name;
+    private String location;
+
+
+    //getters and setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
