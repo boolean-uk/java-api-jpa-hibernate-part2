ad858e33836b173827dde16b901d80ddfe46a604
Emil Solgaard <emil.solgaard@dk.experis.com>
2023-06-06 13:15:16 +0200

    updated openapi-core.yml + added models

---
+++ src/main/java/com/booleanuk/api/models/Author.java
@@ -0,0 +1,81 @@
+package com.booleanuk.api.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "authors")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email_address")
+    private String email;
+    @Column(name = "alive")
+    private boolean alive;
+    @OneToMany(mappedBy = "author")
+    @JsonIgnoreProperties("author")
+    private List<Book> books;
+
+    public Author() {
+        super();
+    }
+    public Author(String firstName, String lastName, String email, boolean alive) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.alive = alive;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
