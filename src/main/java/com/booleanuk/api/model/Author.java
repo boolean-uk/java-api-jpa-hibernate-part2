de282648b1b6ececed436071f8ae80654719cb21
Kevin Huang <krythuang@gmail.com>
2023-10-23 10:11:30 +0200

    Finished assignment

---
+++ src/main/java/com/booleanuk/api/model/Author.java
@@ -0,0 +1,81 @@
+package com.booleanuk.api.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer authorId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "alive")
+    private boolean alive;
+
+    @OneToMany(mappedBy = "author")
+    @JsonIgnoreProperties({"author"})
+    private List<Book> books;
+
+    public Author(){
+    }
+
+    public Author(String firstName, String lastName, String email, boolean alive) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.alive = alive;
+    }
+
+    public Integer getAuthorId() {
+        return this.authorId;
+    }
+
+    public void setAuthorId(Integer authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isAlive() {
+        return this.alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+}
