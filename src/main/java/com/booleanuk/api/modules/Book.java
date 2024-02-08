815fcd73ab307e17358627f5941903fb7674a527
ThomasKvam <thomas.e.h.kvam@hotmail.no>
2024-02-08 12:17:19 +0100

    Created the necessary files. Added variables to the model classes.

---
+++ src/main/java/com/booleanuk/api/modules/Book.java
@@ -0,0 +1,38 @@
+package com.booleanuk.api.modules;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String title;
+    @Column
+    private String genre;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id",nullable = false)
+    @JsonIncludeProperties(value = {"first_name","last_name"})
+    private Author author;
+
+    @ManyToOne
+    @JoinColumn(name = "publisher_id",nullable = false)
+    @JsonIncludeProperties(value = {"name, location"})
+    private Publisher publisher;
+
+    public Book(String title, String genre) {
+        this.title = title;
+        this.genre = genre;
+    }
+}
