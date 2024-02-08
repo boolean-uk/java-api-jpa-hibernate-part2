c53df9db9db4c412b3d9a20c284a6b06e2059cd5
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-08 11:51:20 +0100

    Set up Author class

--- src/main/java/com/booleanuk/api/model/Author.java
+++ src/main/java/com/booleanuk/api/model/Author.java
@@ -1,4 +1,36 @@
 package com.booleanuk.api.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "authors")
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column
+    private String email;
+
+    @Column
+    private boolean alive;
+
+    public Author(String firstName, String lastName, String email, boolean alive) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.alive = alive;
+    }
 }
