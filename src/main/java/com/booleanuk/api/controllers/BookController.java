4d4c62a41b7e00ed42ad179421bc8e568ac5f7b7
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-08 13:13:54 +0100

    Create create endpoint Book

--- src/main/java/com/booleanuk/api/controllers/BookController.java
+++ src/main/java/com/booleanuk/api/controllers/BookController.java
@@ -1,11 +1,13 @@
 package com.booleanuk.api.controllers;
 
 import com.booleanuk.api.model.Book;
+import com.booleanuk.api.repositories.AuthorRepository;
 import com.booleanuk.api.repositories.BookRepository;
+import com.booleanuk.api.repositories.PublisherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -15,8 +17,29 @@ public class BookController {
     @Autowired
     BookRepository repository;
 
+    @Autowired
+    AuthorRepository authorRepository;
+
+    @Autowired
+    PublisherRepository publisherRepository;
+
     @GetMapping
     public List<Book> getAll() {
         return this.repository.findAll();
+    }
+
+    record BookDTO(String title, String genre, int author_id, int publisher_id) {}
+
+    @PostMapping
+    public Book create(@RequestBody BookDTO bookDTO) {
+        Book book = new Book(bookDTO.title, bookDTO.genre);
+
+        book.setAuthor(this.authorRepository.findById(bookDTO.author_id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Author not found")));
+
+        book.setPublisher(this.publisherRepository.findById(bookDTO.publisher_id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Author not found")));
+
+        return this.repository.save(book);
     }
 }
