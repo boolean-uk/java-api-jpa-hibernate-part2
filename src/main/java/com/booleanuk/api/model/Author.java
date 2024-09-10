de4893cf0348197219b787e62efd4a5bf6cbecbd
Stian Rusvik <stian.rusvik@no.experis.com>
2024-09-10 17:58:32 +0200

    added members to Author model.

--- src/main/java/com/booleanuk/api/model/Author.java
+++ src/main/java/com/booleanuk/api/model/Author.java
@@ -1,4 +1,73 @@
 package com.booleanuk.api.model;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "authors")
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "alive")
+    private boolean alive;
+
+
+    public Author(String firstName, String lastName, String email, boolean alive) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.alive = alive;
+    }
+
+    public Author() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
 }
