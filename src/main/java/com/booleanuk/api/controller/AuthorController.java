75e34fb59e351e9ff931c112f4b4b44d020cfd6a
Stian Rusvik <stian.rusvik@no.experis.com>
2024-09-10 17:59:55 +0200

    started implementing author controller.

--- src/main/java/com/booleanuk/api/controller/AuthorController.java
+++ src/main/java/com/booleanuk/api/controller/AuthorController.java
@@ -1,4 +1,44 @@
 package com.booleanuk.api.controller;
 
+import com.booleanuk.api.model.Author;
+import com.booleanuk.api.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("authors")
 public class AuthorController {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+
+    @GetMapping
+    public ResponseEntity<List<Author>> getAllAuthors() {
+        List<Author> result = this.authorRepository.findAll();
+        if (result.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Author> getAuthorById(@PathVariable int id) {
+        return this.authorRepository.findById(id).map(ResponseEntity::ok)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "No author with that id was found"
+                ));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Author createAuthor(@RequestBody Author body) {
+        return this.authorRepository.save(body);
+    }
 }
