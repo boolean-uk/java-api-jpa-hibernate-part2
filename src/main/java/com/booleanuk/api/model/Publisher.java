abefc755acead73575a5a3f4b1228c5b61f77a52
Stian Rusvik <stian.rusvik@no.experis.com>
2024-09-10 18:28:30 +0200

    added publisher model.

--- src/main/java/com/booleanuk/api/model/Publisher.java
+++ src/main/java/com/booleanuk/api/model/Publisher.java
@@ -1,4 +1,66 @@
 package com.booleanuk.api.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "publishers")
 public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "location")
+    private String location;
+
+    @OneToMany(mappedBy = "publisher")
+    @JsonManagedReference
+    private List<Book> books;
+
+    public Publisher(String name, String location) {
+        this.name = name;
+        this.location = location;
+        this.books = new ArrayList<>();
+    }
+
+    public Publisher() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
 }
