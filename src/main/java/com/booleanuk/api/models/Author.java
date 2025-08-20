e88e091ada740bed572573084090a49e54b26924
Moueed Ali <moueed.ali@hotmail.com>
2025-08-20 13:55:36 +0200

    Exercise complete

---
+++ src/main/java/com/booleanuk/api/models/Author.java
@@ -0,0 +1,52 @@
+package com.booleanuk.api.models;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Cascade;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "authors")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "alive")
+    private boolean alive;
+
+    @OneToMany(mappedBy = "author", fetch = FetchType.EAGER)
+    @JsonIncludeProperties({"title", "genre"})
+    private List<Book> books;
+
+    public Author(String firstName, String lastName, String email, boolean alive) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.alive = alive;
+    }
+
+    public Author(int id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/booleanuk/api/models/Publisher.java
@@ -0,0 +1,43 @@
+package com.booleanuk.api.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIncludeProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Cascade;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "publishers")
+public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "location")
+    private String location;
+
+    @OneToMany(mappedBy = "publisher", fetch = FetchType.EAGER)
+    @JsonIncludeProperties({"title", "genre"})
+    private List<Book> books;
+
+    public Publisher(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public Publisher(int id) {
+        this.id = id;
+    }
+}
